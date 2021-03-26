693c9f0e46a94eb6fd29d9b3f663fb6b35340e4b
David Legatt <legattd@ncbpinc.com>
2021-03-26 10:35:26 -0500

    Added HttpClient. Tests updated and passing

--- Hello.php
+++ Hello.php
@@ -2,17 +2,25 @@
 
 namespace Dlegatt\HelloWorld;
 
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
 class Hello
 {
     private $defaultName;
+    /**
+     * @var HttpClientInterface
+     */
+    private $client;
 
-    public function __construct($defaultName)
+    public function __construct($defaultName, HttpClientInterface $client)
     {
         $this->defaultName = $defaultName;
+        $this->client = $client;
     }
 
     public function getGreeting(Greetable $person = null)
     {
+        $this->client->request('GET', '/foo')->getContent();
         $name = (string) ($person ?? $this->defaultName);
         return "Hello, $name!";
     }
--- Tests/HelloTest.php
+++ Tests/HelloTest.php
@@ -5,13 +5,15 @@ namespace Dlegatt\HelloWorld\Tests;
 use Dlegatt\HelloWorld\Hello;
 use Dlegatt\HelloWorld\Person;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpClient\HttpClient;
 
 class HelloTest extends TestCase
 {
     public function testGetGreeting()
     {
         $person = new Person("David");
-        $hello = new Hello("World");
+        $client = HttpClient::createForBaseUri("https://localhost:8000");
+        $hello = new Hello("World", HttpClient::createForBaseUri("https://localhost:8000", ['verify_host' => false, 'verify_peer' => false]));
 
         $this->assertEquals("Hello, David!", $hello->getGreeting($person));
         $this->assertEquals("Hello, World!", $hello->getGreeting());
